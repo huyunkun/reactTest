16390f8cb9b51071f0f1d3ebde64dfaf82a9fa29
zhang <iszhangzhongwei@qq.com>
2018-07-06 17:46:13 +0800

    hyk

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
 
-class App extends Component {
-    render() {
-        return <h1 > Hello, world! </h1>
-    }
-}
+import Home from './pages/Home';
+import './css/index.css'
+import img1 from './images/2.jpg'
 
-ReactDom.render( 
-    <App /> ,
+if (module.hot) {
+    module.hot.accept(() => {
+      ReactDom.render(
+          <AppContainer>
+              <Home />
+          </AppContainer>,
+          document.getElementById('root')
+      )
+    })
+  }
+
+// class App extends Component {
+//     render() {
+//         return <div>
+//                 <h1 className="container"> Hello, world! </h1>
+//                 <img src={img1} alt="图片1"/>
+//                 <h1 className="container"> Hello, world! </h1>
+//         </div>
+        
+//     }
+// }
+
+ReactDom.render(
+    <AppContainer>
+        <Home />
+    </AppContainer>,
     document.getElementById('root')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 
 module.exports = {
     entry: './src/index.js', //入口文件
     output: {
         path: path.resolve(__dirname, 'build'), //打包文件的输出路径
-        filename: 'builde.js'
+        filename: 'builde.js',
+        publicPath:'/'
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build'),
+        publicPath:'/',
+        inline : true,
+        hot:true
     },
     module: {
         rules: [ //配置加载器
@@ -13,16 +21,26 @@ module.exports = {
                 test: /\.js$/, //配置要处理的文件格式，一般使用正则表达式匹配
                 loader: 'babel-loader', //使用的加载器名称
                 query: { //babel的配置参数，可以写在.babelrc文件里也可以写在这里
-                    presets: ['env', 'react']
+                    presets: ['env', 'react'],
+                    plugins: ["react-hot-loader/babel"] //增加
                 }
-            }
+            },
+            {
+                test: /\.css/,//配置处理css文件
+                loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.(jpe?g|png|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/, 
+                loader: 'url-loader?limit=100000',
+            },
+            
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './public/index.html', //制定模板路径
             filename: 'index.html' //指定文件名
-        })
+        }),
     ]
 
 }
